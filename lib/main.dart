a2ae88cbc4a86576e006317dfbdacac8c376e65c
Ignetz <darwinngl88@gmail.com>
2026-06-07 23:46:59 +0700

    tambah splash_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
+import 'screens/splash_screen.dart';
 
-void main() {
-  runApp(const BalaiKuApp());
-}
+void main() => runApp(const BalaiKuApp());
 
 class BalaiKuApp extends StatelessWidget {
   const BalaiKuApp({super.key});
@@ -11,18 +9,12 @@ class BalaiKuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BalaiKu',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF0D6EFD),
-        scaffoldBackgroundColor: const Color(0xFFF8F9FA),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          elevation: 1,
-        ),
+        fontFamily: 'Poppins',
       ),
-      home: const LoginScreen(),
+      home: const SplashScreen(),
     );
   }
 }
